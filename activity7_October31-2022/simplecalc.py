3272f226e21dcae44d8cdf75aa5dcebf6bf8dd21
Samragyi <samragyi1212@gmail.com>
2022-11-02 09:52:27 +0530

    Conversion and Functions

---
+++ activity7_October31-2022/simplecalc.py
@@ -0,0 +1,33 @@
+def addition(num1, num2):
+    num1 += num2
+    return num1
+def subtraction(num1, num2):
+    num1 -= num2
+    return num1
+def mul(num1, num2):
+    num1 *= num2
+    return num1
+def division(num1, num2):
+    num1 /= num2
+    return num1
+def module(num1, num2):
+    num1 %= num2
+    return num1
+def default(num1, num2):
+    return "Incorrect input"
+switcher = {
+1: addition,
+2: subtraction,
+3: mul,
+4: division,
+5: module
+}
+def switch(operation, num1, num2):
+    return switcher.get(operation, default)(num1, num2)
+
+print("Operations \n1. Addition \n2. Subtraction \n3. Multiplication \n4. Division \n5. Module ")
+# Take input from user
+choice = int(input("Select operation from 1,2,3,4 : "))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print (switch(choice, num1, num2))
---
+++ activity7_October31-2022/tempconversion.py
@@ -0,0 +1,13 @@
+# Convert celsius to fahrenheit
+
+c = float(input("Enter the temperature in celsius: "))
+f = (c*1.8)+32
+print(c, "degree celsius is ", f, " degree fahrenheit")
+print()
+
+# Convert fahrenheit to celsius
+
+fr = float(input("Enter the temperature in fahrenheit: "))
+cr = (fr - 32)/1.8
+print(fr, "degree fahrenheit is ", cr, " degree celsius")
+print()
